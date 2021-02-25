986cf1550ecf038853fdb2629dfc364765d423c8
William-Moreno <intrcptr@comcast.net>
2021-02-24 19:02:51 -0800

    server functional

--- src/server.js
+++ src/server.js
@@ -1,66 +1,38 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const notFoundHandler = require('./error-handlers/404.js');
-const errorHandler = require('./error-handlers/500.js');
+const errorHandlerAuth = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/routes.js');
 const logger = require('./middleware/logger.js');
 
 const v1Routes = require('./routes/v1.js');
 
 const app = express();
 
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(logger);
 
+app.use(authRoutes);
 app.use('/api/v1', v1Routes);
 
-app.use('*', notFoundHandler);
-app.use(errorHandler);
+// app.use('*', notFoundHandler);
+// app.use(errorHandler);
+app.use(notFound);
+app.use(errorHandlerAuth);
 
 module.exports = {
   server: app,
   start: port => {
-    if (!port) { throw new Error("Missing Port"); }
+    if (!port) { throw new Error('Missing Port'); }
     app.listen(port, () => console.log(`Listening on ${port}`));
   },
 };
 
-'use strict';
-
-// 3rd Party Resources
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-
-// Esoteric Resources
-const errorHandler = require('./error-handlers/500.js');
-const notFound = require('./error-handlers/404.js');
-const authRoutes = require('./auth/routes.js');
-
-// Prepare the express app
-const app = express();
-
-// App Level MW
-app.use(cors());
-app.use(morgan('dev'));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.use(authRoutes);
-
-// Catchalls
-app.use(notFound);
-app.use(errorHandler);
-
-module.exports = {
-  server: app,
-  start: (port) => {
-    app.listen(port, () => {
-      console.log(`Server Up on ${port}`);
-    });
-  },
-};
