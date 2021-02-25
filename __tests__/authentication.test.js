94c629a91c3e753019fb0d950ce5762f983a7d2b
William-Moreno <intrcptr@comcast.net>
2021-02-25 12:53:13 -0800

    Revert "removed redundant test file"
    This reverts commit f12d94061f603d732beaacccf5c497eb90f0d374.

---
+++ __tests__/authentication.test.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.env.SECRET = 'toes';
+
+const server = require('../src/server.js').server;
+const supergoose = require('@code-fellows/supergoose');
+const bearer = require('../src/auth/middleware/bearer.js');
+
+const mockRequest = supergoose(server);
+
+let users = {
+  admin: { username: 'admin', password: 'password', role: 'admin' },
+  editor: { username: 'editor', password: 'password', role: 'editor' },
+  user: { username: 'user', password: 'password' },
+};
+
+describe('Auth Router', () => {
+
+  Object.keys(users).forEach(userType => {
+
+    describe(`${userType} users`, () => {
+
+      it('can create one', async () => {
+
+        const response = await mockRequest.post('/signup').send(users[userType]);
+        const userObject = response.body;
+
+        expect(response.status).toBe(201);
+        expect(userObject.token).toBeDefined();
+        expect(userObject.user._id).toBeDefined();
+        expect(userObject.user.username).toEqual(users[userType].username);
+
+      });
+
+      it('can signin with basic', async () => {
+
+        const response = await mockRequest.post('/signin')
+          .auth(users[userType].username, users[userType].password);
+
+        const userObject = response.body;
+        expect(response.status).toBe(200);
+        expect(userObject.token).toBeDefined();
+        expect(userObject.user._id).toBeDefined();
+        expect(userObject.user.username).toEqual(users[userType].username);
+
+      });
+
+    });
+
+  });
+
+});
